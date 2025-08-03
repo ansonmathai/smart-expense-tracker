336162451ff562eca9174ca2e7fc71ce04df2bed
Anson Mathai <mathaianson@gmail.com>
2025-08-03 09:16:18 +0530

    Filtering and Sorting commit

--- src/main/java/com/anson/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/anson/expensetracker/controller/ExpenseController.java
@@ -3,6 +3,7 @@ package com.anson.expensetracker.controller;
 import com.anson.expensetracker.dto.ExpenseFilter;
 import com.anson.expensetracker.model.Expense;
 import com.anson.expensetracker.repository.ExpenseRepository;
+import com.anson.expensetracker.service.ExpenseService;
 
 import jakarta.validation.Valid;
 
@@ -12,12 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 
@@ -29,6 +24,9 @@ public class ExpenseController {
 
     @Autowired
     private ExpenseRepository expenseRepository;
+
+    @Autowired
+    private ExpenseService expenseService;
 
     @GetMapping
     public List<Expense> getAllExpenses() {
